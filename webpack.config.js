4b15192867ef0bf17da3cd5479aaa8e25af5106c
Stanislav <delfi89@gmail.com>
2023-12-17 19:41:52 +0300

    chore: настройка сборки

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,63 @@
 const path = require("path");
+const sveltePreprocess = require("svelte-preprocess");
+
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const sveltePreprocess = require("svelte-preprocess");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const SvelteCheckPlugin = require("svelte-check-plugin");
 
 module.exports = (env) => {
   const isDev = !!(env && env.development);
 
+  const projectExtensions = [".mjs", ".js", ".ts", ".svelte"];
+
   return {
     mode: isDev ? "development" : "production",
-    entry: {
-      "build/bundle": ["./src/main.ts"],
-    },
+    entry: path.join(__dirname, "src/main.ts"),
     output: {
-      path: path.join(__dirname, "/public"),
-      filename: "[name].js",
+      clean: true,
+      path: path.join(__dirname, "/build"),
+      filename: "[contenthash].js",
       chunkFilename: "[name].[id].js",
     },
     resolve: {
-      plugins: [new TsconfigPathsPlugin()],
-      // alias: {
-      //   "~": path.resolve(__dirname, "src"),
-      //   svelte: path.resolve(__dirname, "node_modules", "svelte/src/runtime"),
-      // },
-      // extensions: [".mjs", ".js", ".svelte"],
-      // mainFields: ["svelte", "browser", "module", "main"],
-      // conditionNames: ["svelte", "browser", "import"],
+      plugins: [
+        new TsconfigPathsPlugin({
+          extensions: projectExtensions,
+        }),
+      ],
+      extensions: projectExtensions,
+      mainFields: ["svelte", "browser", "module", "main"],
+      conditionNames: ["svelte", "browser", "import"],
+      alias: {
+        svelte: path.join(__dirname, "node_modules", "svelte/src/runtime"),
+      },
     },
     module: {
       rules: [
         {
-          test: /\.m?js$/,
+          test: /\.html$/,
+          loader: "html-loader",
+        },
+        {
+          test: /\.ts$/,
+          use: "ts-loader",
+          exclude: path.join(__dirname, "node_modules"),
+        },
+        {
+          test: /\.(?:js|mjs|cjs)$/,
           exclude: /(node_modules|bower_components)/,
           use: {
             loader: "babel-loader",
+            options: {
+              presets: [["@babel/preset-env", { targets: "defaults" }]],
+            },
           },
         },
         {
-          test: /\.(html|svelte)$/,
+          test: /\.svelte$/,
           use: {
             loader: "svelte-loader",
             options: {
@@ -74,12 +95,24 @@ module.exports = (env) => {
         },
       ],
     },
-    plugins: [new MiniCssExtractPlugin({ filename: "./[name].css" })],
+    plugins: [
+      new MiniCssExtractPlugin({ filename: "./[contenthash].css" }),
+      new HtmlWebpackPlugin({ template: "./public/index.html" }),
+      new SvelteCheckPlugin(),
+    ],
     devServer: {
       hot: true,
       static: {
-        directory: path.join(__dirname, "public"),
+        watch: true,
+        directory: path.join(__dirname, "build"),
       },
     },
+    optimization: !isDev
+      ? {
+          minimize: true,
+          usedExports: true,
+          minimizer: [new TerserPlugin({ parallel: true }), new CssMinimizerPlugin()],
+        }
+      : {},
   };
 };
